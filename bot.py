fb71a00d69cc83de00436ca71b0077b6f4ed3965
guy56890 <Enchantion1@gmail.com>
2025-09-26 13:02:36 +0200

    redeploy implementation

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import random
+import aiohttp
 import asyncio
 import discord
 from discord import app_commands
@@ -7,6 +8,7 @@ from discord.ext import commands
 from datetime import timedelta
 
 TOKEN = os.getenv("DISCORD_TOKEN")
+WEBHOOK_URL = os.getenv("PORTAINER_WEBHOOK")
 
 intents = discord.Intents.default()
 intents.guilds = True
@@ -27,6 +29,23 @@ async def on_ready():
         print(e)
     print(f"Logged in as {bot.user}")
 
+@bot.tree.command(name="re-deploy", description="Forcefully update the bot to the newest commit on GitHub")
+async def redeploy(interaction: discord.Interaction):
+
+    if interaction.user.id != 554691397601591306:  # Replace with your Discord user ID
+        await interaction.response.send_message("You do not have permission to use this command.", ephemeral=True)
+        return
+
+    await interaction.response.defer(ephemeral=True)
+    await interaction.response.send_message("Triggerer deploy… 🚀")
+    async with aiohttp.ClientSession() as session:
+        async with session.post(WEBHOOK_URL) as resp:
+            if resp.status == 200:
+                await interaction.response.send_message("Portainer webhook succes — opdatering startet.")
+            else:
+                text = await resp.text()
+                await interaction.response.send_message(f"Fejl: {resp.status} — {text}")
+
 @bot.tree.command(name="coinflip", description="Flip a coin for admin or timeout")
 async def coinflip(interaction: discord.Interaction):
     await interaction.response.defer(ephemeral=True)
@@ -40,15 +59,4 @@ async def coinflip(interaction: discord.Interaction):
         return
     
     await interaction.followup.send(f"Flipping a coin for {member.mention}...")
-
-@bot.tree.command(name="test", description="this is a new test command")
-async def coinflip(interaction: discord.Interaction):
-    await interaction.response.defer(ephemeral=True)
-
-    member = interaction.user
-    guild = interaction.guild
-    admin_role = guild.get_role(ADMIN_ROLE_ID)
-
-    
-    await interaction.followup.send(f"doo be doo")
 bot.run(TOKEN)
